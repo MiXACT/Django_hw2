2fa73021d8f71096d58cd9ae1d0313cb734e353e
MiXACT <out_lawz@mail.ru>
2022-02-08 21:34:49 +0300

    recipe mult

---
+++ calculator/views.py
@@ -0,0 +1,44 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'butter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+}
+
+def recipe_multiplier(request, recipe):
+    servings = int(request.GET.get('servings', 1))
+    for ingredient in recipe:
+        recipe[ingredient] *= servings
+    return recipe
+
+def omlet_view(request):
+    data_copy = DATA['omlet'].copy()
+    omlet = recipe_multiplier(request, data_copy)
+    context = {'recipe': omlet}
+    return render(request, 'calculator/index.html', context)
+
+def pasta_view(request):
+    data_copy = DATA['pasta'].copy()
+    pasta = recipe_multiplier(request, data_copy)
+    context = {'recipe': pasta}
+    return render(request, 'calculator/index.html', context)
+
+def butter_view(request):
+    data_copy = DATA['butter'].copy()
+    butter = recipe_multiplier(request, data_copy)
+    context = {'recipe': butter}
+    return render(request, 'calculator/index.html', context)
